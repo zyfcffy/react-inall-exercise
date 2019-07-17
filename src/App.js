0804823385aaf8a523de32420de854d495175dc6
杜娟 <jdu.xian@gmail.com>
2019-07-17 22:38:28 +0800

    use less

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import './App.less';
 
 class App extends Component{
   render() {
     return (
-      <div>Hello World</div>
+      <div className='App'>Hello World</div>
     );
   }
 }
